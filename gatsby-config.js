8f9b65f60d614c2546f14c8855aedf92522be06c
Flancian <0@flancia.org>
2020-10-14 09:07:41 +0200

    Lock gatsby to old version to work around https://github.com/mathieudutour/gatsby-digital-garden/issues/50.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,13 @@ module.exports = {
       resolve: `gatsby-theme-garden`,
       options: {
         // basePath: `${__dirname}`,
-        contentPath: `${__dirname}/agora`,
+        contentPath: `${__dirname}/agora/garden/flancian/`,
         rootNote: `/index`,
       },
     },
   ],
   siteMetadata: {
     title: `An Agora`,
+    author: `the Agoreans`,
   },
 }
